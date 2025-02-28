7937246cf05ea8b846a9a7d30107f1e104fb80f5
nsharma123 <nsharma123@users.noreply.github.com>
2025-02-28 10:04:02 +0000

    Deploy preview for PR 11 🛫

--- pr/pr-11/demo/app-root.js
+++ pr/pr-11/demo/app-root.js
@@ -1260,12 +1260,12 @@
         <hr />
         <ul>
           <li>
-            <a href="https://pi.archive.org/control/setadmin.php"
+            <a href="https://catalogd.archive.org/control/setadmin.php"
               >Grant or revoke administrator privileges</a
             >
           </li>
           <li>
-            <a href="https://pi.archive.org/control/useradmin.php"
+            <a href="https://catalogd.archive.org/control/useradmin.php"
               >User Administration</a
             >
           </li>
